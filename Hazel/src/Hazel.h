e717d3bd374e63aad8b67d4d3ab48be6cd115d21
ysyinsen <1934096706@qq.com>
2025-07-04 10:10:52 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//For use by Hazel Application
+
+#include "Hazel/Application.h"
+
+//------------EntityPoint-------------
+#include "Hazel/EntityPoint.h"
+//------------------------------------
